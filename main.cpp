3c26f1586ddbb880282b88c2542b3bd179f7ae7e
Lucas Tjor <tjor75@gmail.com>
2026-08-05 18:32:37 -0300

    Poner primera opcion amarilla al iniciar menu

--- main.cpp
+++ main.cpp
@@ -797,10 +797,7 @@ namespace Menues
   void obtenerOpcionSeleccionada(int &opcionSeleccionada, str24 opciones[], short numOpciones)
   {
     for (int i = 0; i < numOpciones; i++)
-    {
-      _gotoxy(5, i + 1);
-      cout << opciones[i];
-    }
+      MnsgBox(5, i + 1, i == 0 ? AMARILLO, BLANCO_BRILLANTE, opciones[i]);
     opcionSeleccionada = MenuNavegar(opciones, 1, numOpciones, 5);
   }
 
